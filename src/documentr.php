10108d1ce3bc48fafb51b81bc0bed4f7d9b07b73
masterexploder <ian@gen-x-design.com>
2010-02-10 19:50:36 -0800

    more work on the index page and default theme

--- src/documentr.php
+++ src/documentr.php
@@ -30,3 +30,19 @@ foreach ($config['guides'] as $section => $group)
 	
 	$index[$section] = $indexArray;
 }
+
+// we've passed the config / parse, so we'll clear the output directory
+shell_exec('rm -rf ' . $config['output_dir'] . '/*');
+shell_exec('cp -f ' . DOCUMENTR_ROOT . '/templates/' . $config['template'] . '/styles.css ' . $config['output_dir']);
+shell_exec('cp -rf ' . DOCUMENTR_ROOT . '/templates/' . $config['template'] . '/images ' . $config['output_dir']);
+
+// build the home page
+ob_start();
+include  DOCUMENTR_ROOT . '/templates/' . $config['template'] . '/home.php';
+$contents = ob_get_contents();
+ob_end_clean();
+
+// and write it...
+$handle = fopen($config['output_dir'] . '/index.html', 'w');
+fwrite($handle, $contents);
+fclose($handle);
